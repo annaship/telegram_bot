5bfa9331fbfdd2d131673e2d7982a8affe915919
annaship <tetya-loshad@yandex.ru>
2022-08-28 17:53:06 -0400

    correct output

--- test_wiki2.py
+++ test_wiki2.py
@@ -2,16 +2,19 @@
 
 import pywikibot
 import sys
+
 print("Argument List: %s!" % str(sys.argv))
 
 site = pywikibot.Site()
 page = pywikibot.Page(site, u"Тестирую_wikibot_APIs")
-text = page.text
+text = page.text 
 
 args_text = ' '.join(sys.argv[1:])
-page.text = text + "<br>new text: " + ' '.join(args_text)
-# new text: F o r w a r d e d f r o m B o t F a t h e r
+print("args_text: %s" % str(args_text))
 
+page.text = text + "<br>new text: " + str(args_text)
 
 # u"newText!"
 page.save(u"Edit comment")
+
+
