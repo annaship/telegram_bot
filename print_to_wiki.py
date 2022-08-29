be9fbcacf22124e5c7abc00893c396080d3777f4
annaship <tetya-loshad@yandex.ru>
2022-08-29 13:06:17 -0400

    use encode

--- print_to_wiki.py
+++ print_to_wiki.py
@@ -1,16 +1,22 @@
-# /usr/bin/python3
+#!/usr/bin/python3
 
 import pywikibot
 import sys
+import base64
 
-print("Argument List: %s!" % str(sys.argv))
+# print("Argument List: %s!" % str(sys.argv))
 
 site = pywikibot.Site()
 page = pywikibot.Page(site, u"Ссылки_из_Телеграма_на_разбор")
 text = page.text 
 
 args_text = ' '.join(sys.argv[1:])
+args_text = args_text.replace("\\r\\n", "<br>").replace("\\n", "<br>")
 print("args_text: %s" % str(args_text))
+
+Str = base64.b64encode(args_text.encode('utf-8', errors = 'strict'))
+
+print ("Encoded String: " , Str)
 
 page.text = text + "<br>new text: " + str(args_text)
 
