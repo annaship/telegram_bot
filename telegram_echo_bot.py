e497ddc10cf5ae5a326586b2d774398fb0d924f4
annaship <tetya-loshad@yandex.ru>
2022-08-30 13:45:07 -0400

    clean up

--- telegram_echo_bot.py
+++ telegram_echo_bot.py
@@ -3,14 +3,12 @@
 # This program is dedicated to the public domain under the CC0 license.
 
 """
-Simple Bot to reply to Telegram messages.
-
+Changed from the Echo example by A. Shipunova Aug 2022 
 First, a few handler functions are defined. Then, those functions are passed to
 the Dispatcher and registered at their respective places.
 Then, the bot is started and runs until we press Ctrl-C on the command line.
 
 Usage:
-Basic Echobot example, repeats messages.
 Press Ctrl-C on the command line or send a signal to the process to stop the
 bot.
 """
@@ -48,7 +46,6 @@ def help_command(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /help is issued."""
     update.message.reply_text('Help me!')
 
-
 def echo_command(update: Update, context: CallbackContext) -> None:
     """Echo the user message."""
     logger.info(update)
@@ -57,9 +54,6 @@ def echo_command(update: Update, context: CallbackContext) -> None:
 def send2wiki(update: Update, context: CallbackContext) -> None:
     """Send to wiki the user message."""
     logger.info(update)
-    # os.system("cd ~/software/core_stable; python3 pwb.py login")
-    # os.system("python3 /home/rubikus/software/bots/print_to_wiki.py " + update.message.text)  
-    # print(update.message.text)
 
     msg_id = str(update.message.message_id) 
     text2send = msg_id + " ## " + update.message.text
@@ -67,15 +61,10 @@ def send2wiki(update: Update, context: CallbackContext) -> None:
     text2send = unicodedata.normalize('NFC', text2send)
     cmd = 'python3 /home/rubikus/software/bots/print_to_wiki.py ' + text2send
     # print("CMD: %s" % cmd)
-
     args2send = shlex.split(cmd)
 
-    # p = subprocess.Popen(args2send)
-    #out = subprocess.check_output(args2send)
-    # print(out.decode('ascii')) # 'out' is a byte string
     p = subprocess.run(args2send)
 
-    # update.message.reply_text(update.message.text)
 
 def main() -> None:
     """Start the bot."""
@@ -88,11 +77,8 @@ def main() -> None:
     # on different commands - answer in Telegram
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", help_command))
-    # dispatcher.add_handler(CommandHandler("echo", echo_command))
 
-    # on non command i.e message - echo the message on Telegram
-    # dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
-    # dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo_command))
+    # on non command i.e message - send the message to Wiki
     dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, send2wiki))
 
     # Start the Bot
