36f126b1853bbe1fc442ceafac9697e9c0b74dc0
annaship <tetya-loshad@yandex.ru>
2022-09-06 12:24:37 -0400

    err
    NameError: name 'parts' is not defined. Did you mean: 'parse'?

--- varenik2wiki.py
+++ varenik2wiki.py
@@ -4,7 +4,19 @@ import pywikibot
 import json
 import pprint
 #from urllib.request import urlopen
-import urllib
+
+from urllib import parse
+from urllib import request
+
+varenik_json_url = 'https://opensheet.elk.sh/1lWvfxzdlWNxWuWCW1rRWy4TON-XIuE4jOmlJmq5SYNA/%D0%9F    %D0%BE%D0%BB%D0%BD%D0%B0%D1%8F'
+
+parts - parse.urlsplit(varenik_json_url)
+query_dict = parse.parse_qs(parts.query)
+encoded_query = parse.urlencode(query_dict)
+fixed_url = parse.urlunsplit((parts.scheme, parts.netloc, parts.path, encoded_query, parts.fragment))
+response = request.urlopen(fixed_url)
+
+print(json.load(response))
 
 # site = pywikibot.Site()
 # page = pywikibot.Page(site, u"Varenik json")
@@ -12,31 +24,6 @@ import urllib
 
 # print("Argument List: %s!" % str(sys.argv))
 
-varenik_json_url = 'https://opensheet.elk.sh/1lWvfxzdlWNxWuWCW1rRWy4TON-XIuE4jOmlJmq5SYNA/%D0%9F%D0%BE%D0%BB%D0%BD%D0%B0%D1%8F'
-str_enc = varenik_json_url.encode('utf-8')
-
-from socket import timeout
-try:
-    response = urllib.request.urlopen(str_enc, timeout=10).read().decode('utf-8')
-#except HTTPError as error:
-#    logging.error('HTTP Error: Data of %s not retrieved because %s\nURL: %s', name, error, url)
-#except URLError as error:
-#    if isinstance(error.reason, timeout):
-#        logging.error('Timeout Error: Data of %s not retrieved because %s\nURL: %s', name, error, url)
-except Exception as inst:
-        print('Error:  %s not retrieved \nURL: %s', inst, str_enc)
-    #else:
-        #logging.error('URL Error: Data of %s not retrieved because %s\nURL: %s', name, error, url)
-else:
-    print('Access successful.')
-
-#with urlopen(str_enc) as resp:
-#  project_info = json.load(resp)
-
-#with urlopen('https://pypi.org/pypi/sampleproject/json') as resp:
-#    project_info = json.load(resp)['info']
-
-#pprint.pprint(response)
 # page.save(u"Bot insert")
 
 
